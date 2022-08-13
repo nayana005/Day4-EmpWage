0762946cb3138b817ca1890dd02c0e82b2ad6c66
nayana005 <nayanasuresh1005@gmail.com>
2022-08-13 20:18:48 +0530

    Added: EmpWage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,15 +1,21 @@
 package com.bl.emp_wage_computation;
 
 public class EmpWageComputation {
-	public static void main (String[] args) {
-		int IS_FULL_TIME = 1;
-  
-		int empCheck = (int)Math.floor(Math.random() * 10 ) % 2;
+	public static final int IS_FULL_TIME = 1;
+	public static final int EMP_RATE_PER_HR = 20;
 
-		if(empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is Present.");
+	public static void main (String[] args) {  
+		int empHrs = 0; 
+		int empWage = 0;
+		int empCheck = (int)Math.floor(Math.random() * 10 ) % 2; 
+		if(empCheck == IS_FULL_TIME) {	
+			empHrs = 8;
+			empWage = empHrs * EMP_RATE_PER_HR;
+			System.out.println("Employee is Present." + " Employee Wage is : " + empWage);
 		}
 		else
-			System.out.println("Employee is Absent.");
+		{
+			System.out.println("Employee is Absent.");	
+		}
 	}
 }
